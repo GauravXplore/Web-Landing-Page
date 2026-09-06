e2f73d5d3c475dc2905f508b522c8a9f5283c62b
User <user@example.com>
2026-09-06 18:44:20 +0530

    commit

--- 21-6/newfile/hi.java
+++ 21-6/newfile/hi.java
@@ -1,16 +1,13 @@
 import java.util.*;
 class hawww{
-    /**
-     * @param args
-     */
-    public static void main(String[]args){
+        public static void main(String[]args){
         String name = "Columbina";
-        String fullname ="Columbina Hyposelenia";
         String sentence="My name is Columbina Hyposelenia";
         System.out.println(sentence);
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        name= sc.nextLine();
+        try (var in = new Scanner(System.in)) {
+            System.out.print("Enter your name: ");
+            name= in.nextLine();
+        }
         System.out.println("Your name is "+ name);
     }
 }
