836bf16411e3ae7ba22fd6cf3fad57fe49e65c17
User <user@example.com>
2026-09-10 22:37:38 +0530

    Commit

--- 21-6/hi.java
+++ 21-6/hi.java
@@ -4,10 +4,10 @@ class hawww{
         String name = "Columbina";
         String sentence="My name is Columbina Hyposelenia";
         System.out.println(sentence);
-        try (var in = new Scanner(System.in)) {
-            System.out.print("Enter your name: ");
-            name= in.nextLine();
-        }
-        System.out.println("Your name is "+ name);
+        try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter your name: ");
+            name= sc.nextLine();
+		} 
+            System.out.println("Your name is "+ name);
     }
 }
